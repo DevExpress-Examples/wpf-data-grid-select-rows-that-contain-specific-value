028e94254db920ff6a19389e18ec181271e18413
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_SelectRows/Window1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace DXGrid_SelectRows {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            grid.ItemsSource = ProductList.GetData();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            SelectProducts(20);
+        }
+
+        private void SelectProducts(double minPrice) {
+            tableView.BeginSelection();
+            tableView.ClearSelection();
+            for (int i = 0; i < grid.VisibleRowCount; i++) {
+                int rowHandle = grid.GetRowHandleByVisibleIndex(i);
+                if (!grid.IsGroupRowHandle(rowHandle)) {
+                    double price = (double)grid.GetCellValue(rowHandle,
+                        grid.Columns["UnitPrice"]);
+                    if (price >= minPrice)
+                        tableView.SelectRow(rowHandle);
+                }
+            }
+            tableView.EndSelection();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to select rows that contain the specified value
+
+
+<p>This example shows how to select rows whose 'UnitPrice' column contains a value equal to or greater than 20. To select rows, click the 'Select' button.</p>
+
+<br/>
+
+
