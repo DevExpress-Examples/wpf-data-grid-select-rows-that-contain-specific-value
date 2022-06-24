0d87dc527a31419cd2d0f81ea51083995b8e1b03
DevExpressExampleBot <fakeEmail>
2022-06-24 17:46:59 +0400

    autogenerate VB

--- VB/DXGrid_SelectRows/Window1.xaml.vb
+++ VB/DXGrid_SelectRows/Window1.xaml.vb
@@ -1,31 +1,12 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace DXGrid_SelectRows
-	Partial Public Class Window1
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = ProductList.GetData()
-		End Sub
+    Public Partial Class Window1
+        Inherits Window
 
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			SelectProducts(20)
-		End Sub
-
-		Private Sub SelectProducts(ByVal minPrice As Double)
-			grid.BeginSelection()
-			grid.UnselectAll()
-			For i As Integer = 0 To grid.VisibleRowCount - 1
-				Dim rowHandle As Integer = grid.GetRowHandleByVisibleIndex(i)
-				If Not grid.IsGroupRowHandle(rowHandle) Then
-					Dim price As Double = CDbl(grid.GetCellValue(rowHandle, grid.Columns("UnitPrice")))
-					If price >= minPrice Then
-						grid.SelectItem(rowHandle)
-					End If
-				End If
-			Next i
-			grid.EndSelection()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
