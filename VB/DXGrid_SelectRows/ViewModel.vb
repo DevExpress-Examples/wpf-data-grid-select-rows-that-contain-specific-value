bcf57a7281d5023cdbe36ac8a8c096b6dde54f75
DevExpressExampleBot <fakeEmail>
2022-06-24 18:58:53 +0400

    Source auto update [skip ci]

---
+++ VB/DXGrid_SelectRows/ViewModel.vb
@@ -0,0 +1,47 @@
+Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.DataAnnotations
+Imports System.Collections.ObjectModel
+Imports System.Linq
+
+Namespace DXGrid_SelectRows
+
+    Public Class ViewModel
+        Inherits ViewModelBase
+
+        Public Sub New()
+            Products = ProductList.GetData()
+            Selection = New ObservableCollection(Of Product)() From {Products.ElementAt(0)}
+        End Sub
+
+        Public Property Products As ObservableCollection(Of Product)
+            Get
+                Return GetValue(Of ObservableCollection(Of Product))()
+            End Get
+
+            Set(ByVal value As ObservableCollection(Of Product))
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property Selection As ObservableCollection(Of Product)
+            Get
+                Return GetValue(Of ObservableCollection(Of Product))()
+            End Get
+
+            Set(ByVal value As ObservableCollection(Of Product))
+                SetValue(value)
+            End Set
+        End Property
+
+        <Command>
+        Public Sub SelectRows(ByVal textValue As String)
+            Dim value = Double.Parse(textValue)
+            Selection = New ObservableCollection(Of Product)(From item In Products Where item.UnitPrice >= value Select item)
+        End Sub
+
+        Public Function CanSelectRows(ByVal textValue As String) As Boolean
+            Dim value As Double = Nothing
+            Return Double.TryParse(textValue, value)
+        End Function
+    End Class
+End Namespace
---
+++ VB/DXGrid_SelectRows/Window1.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DXGrid_SelectRows
+
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
