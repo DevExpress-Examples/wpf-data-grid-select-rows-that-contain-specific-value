2c406c32dfc8ba07c484f507cca589a83b1f8317
DevExpressExampleBot <fakeEmail>
2022-11-03 16:12:18 +0400

    Source auto update [skip ci]

--- VB/DXGrid_SelectRows/Window1.xaml.vb
+++ VB/DXGrid_SelectRows/Window1.xaml.vb
@@ -1,32 +1,31 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 
 Namespace DXGrid_SelectRows
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = ProductList.GetData()
-		End Sub
 
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			SelectProducts(20)
-		End Sub
+    Public Partial Class Window1
+        Inherits Window
 
-		Private Sub SelectProducts(ByVal minPrice As Double)
-            grid.BeginSelection()
-            grid.UnselectAll()
-			For i As Integer = 0 To grid.VisibleRowCount - 1
-				Dim rowHandle As Integer = grid.GetRowHandleByVisibleIndex(i)
-				If (Not grid.IsGroupRowHandle(rowHandle)) Then
-					Dim price As Double = CDbl(grid.GetCellValue( _
-						rowHandle, grid.Columns("UnitPrice")))
-					If price >= minPrice Then
-                        grid.SelectItem(rowHandle)
-					End If
-				End If
-			Next i
-            grid.EndSelection()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = ProductList.GetData()
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            SelectProducts(20)
+        End Sub
+
+        Private Sub SelectProducts(ByVal minPrice As Double)
+            Me.grid.BeginSelection()
+            Me.grid.UnselectAll()
+            For i As Integer = 0 To Me.grid.VisibleRowCount - 1
+                Dim rowHandle As Integer = Me.grid.GetRowHandleByVisibleIndex(i)
+                If Not Me.grid.IsGroupRowHandle(rowHandle) Then
+                    Dim price As Double = CDbl(Me.grid.GetCellValue(rowHandle, Me.grid.Columns("UnitPrice")))
+                    If price >= minPrice Then Me.grid.SelectItem(rowHandle)
+                End If
+            Next
+
+            Me.grid.EndSelection()
+        End Sub
+    End Class
 End Namespace
